b66d9ed75287b7f3434253232e4daf868f5e3d5d
nakyeong <rinarina0429@naver.com>
2023-11-03 09:46:22 +0900

    [REFACTOR] MemberController.java URL 수정

--- second-springboot/src/main/java/org/sopt/www/firstspringboot/controller/MemberController.java
+++ second-springboot/src/main/java/org/sopt/www/firstspringboot/controller/MemberController.java
@@ -14,7 +14,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/member")
+@RequestMapping("/api/members")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
